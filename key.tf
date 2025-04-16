6c182ac0897f36acae7104dff960abc0dc6bd518
jasleendhir11 <jkourdhir@gmail.com>
2025-04-15 22:27:58 -0400

    Update key.tf

--- key.tf
+++ key.tf
@@ -1,4 +1,5 @@
-######################################### Key pair
+# Creating an AWS EC2 Key pair...
+
 resource "aws_key_pair" "asg_key" {
   key_name   = "asg-key"
   public_key = file("./my_key.pub")
